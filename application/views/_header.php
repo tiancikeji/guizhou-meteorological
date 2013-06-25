9d054d1173a2bfc87732d255873dabb596282512
王雷 <wanglei@chinacache.com>
2013-06-25 15:21:40 +0800

    update

---
+++ application/views/_header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>贵州气象在线 - 手机版</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" /> 
+<meta name="format-detection" content="telephone=no" />
+<meta name="keywords" content="贵州天气网 | 贵州天气预报 | 贵州天气实况 | 贵州预警" />
+<meta name="description" content="贵州气象在线是集贵州省天气预报，天气实况，气象预警预警，科普信息与一体的天气信息门户网站，旨在为人们提供最新最权威的气象信息。" />
+<link rel="Shortcut icon" href="/resources/images/favicon.ico" />
+<link type="text/css" rel="stylesheet" href="/resources/styles/style.css" />
+</head>
+<body>
+
+	<div class="nav">
+    <a class="active" href="/alarm">预警信息</a>
+    <a href="/forecast">天气预报</a>
+    <a href="/weather">天气实况</a>
+    <a class="nborder" href="/news">天气新闻</a>
+	</div><!-- nav -->
+
--- application/views/weather.php
+++ application/views/weather.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<title>天气实况 - 手机贵州气象在线</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" /> 
-<meta name="format-detection" content="telephone=no" />
-<meta name="keywords" content="贵州天气网 | 贵州天气预报 | 贵州天气实况 | 贵州预警" />
-<meta name="description" content="贵州气象在线是集贵州省天气预报，天气实况，气象预警预警，科普信息与一体的天气信息门户网站，旨在为人们提供最新最权威的气象信息。" />
-<link rel="Shortcut icon" href="resources/images/favicon.ico" />
-<link type="text/css" rel="stylesheet" href="resources/styles/style.css" />
-</head>
-<body>
 
-	<div class="nav">
-		<a href="alarm.html">预警信息</a><a href="forecast.html">天气预报</a><a class="active" href="weather.html">天气实况</a><a class="nborder" href="news.html">天气新闻</a>
-	</div><!-- nav -->
-
+  <?php $this->load->view('_header');?>  
 	<div class="search">
 		<form action="">
 			<input name="keyword" type="text" id="keyword" class="s-hot" size="10" value="贵阳" />
@@ -58,12 +42,4 @@
 		</div>
 	</div>
 
-	<div class="footer">
-		贵州省气象局@版权所有 <a href="http://www.gzqx.gov.cn/">WWW.GZQX.GOV.CN</a><br />
-		贵州省气象信息中心技术支持<br />维护支持电话：0851－5202278<br />
-		单位地址：贵州省贵阳市新华路翠微巷9号<br />
-		黔ICP备字030025号
-	</div><!-- footer -->
-
-</body>
-</html>
+  <?php $this->load->view('_footer');?>  
