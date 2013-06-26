2f81c882ebeb68c3e24b03dddce14bdbe8483374
王雷 <wanglei@chinacache.com>
2013-06-26 16:36:59 +0800

    update

--- application/views/news.php
+++ application/views/news.php
@@ -1,18 +1,24 @@
 
   <?php $this->load->view('_header');?>  
-	<div class="content">
-		<table class="table-list">
-			<tbody>
-      <?php var_dump($query); ?>
-				<tr>
-					<td class="s-pic"><a href="/news/details"><img class="a-pic" src="resources/images/news.jpg" alt="图片" /></a></td>
-					<td class="s-info">
-						<div class="news-title"><a href="/news/details">毕节市委书记张吉勇在气象平台值班速记</a></div>
-						<div class="news-date"><span class="time">2013-06-23 22:53:05</span></div>
-					</td>
-				</tr>
-			</tbody>
-		</table>
-	</div><!-- list -->
+  <div class="content">
+    <table class="table-list">
+      <tbody>
+<?php foreach($query as $value){
+?>
+        <tr>
+          <td class="s-pic"><a href="/news/details/<?php echo $value->id;?>"><img class="a-pic" src="<?php echo $value->litpic;  ?>" alt="图片" /></a></td>
+          <td class="s-info">
+            <div class="news-title">
+                <a href="/news/details/<?php echo $value->id;?>"><?php echo $value->title; ?></a>
+            </div>
+            <div class="news-date"><span class="time"><?php echo date('Y-m-d',$value->pubdate);?></span></div>
+          </td>
+        </tr>
+<?php
+}
+?>
+      </tbody>
+    </table>
+  </div><!-- list -->
 
   <?php $this->load->view('_footer');?>  
